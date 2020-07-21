ea6d56d527bfbd061aebf438d2e7dea206352344
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2020-07-21 13:19:15 -0500

    Communication between components

--- components/hijo.js
+++ components/hijo.js
@@ -1,11 +1,11 @@
 Vue.component('hijo',{
 
-    template: //html
+    template:
     `
     <div class='py-6 bg-success'>
-    <h4>Componente hijo {{numero}}</h4>
-    <h4>Nombre : {{nombre}}</h4>
-    <button @click="numero++">+</button>
+        <h4>Componente hijo {{numero}}</h4>
+        <h4>Nombre : {{nombre}}</h4>
+        <button @click="numero++">+</button>
     </div> 
     
     `,
@@ -16,6 +16,6 @@ Vue.component('hijo',{
         }
     },
     mounted(){
-        this.$emit('nnombreHijo',this.nombre);
+        this.$emit('nombreHijo',this.nombre);
     }
 })
--- components/padre.js
+++ components/padre.js
@@ -1,12 +1,12 @@
 Vue.component('padre',{
-
-    template: //html
+    //Los eventos se reciben con @
+    template: 
     `
     <div class="p-5 bg-dark text-white">
-    <h2>Componente Padre: {{numeroPadre}}</h2>
-    <button class="btn btn-danger" @click="numeroPadre++">+</button>
-    {{nombrePadre}}
-    <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
+        <h2>Componente Padre: {{numeroPadre}}</h2>
+        <button class="btn btn-danger" @click="numeroPadre++">+</button>
+        {{nombrePadre}}
+        <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
     </div>
      `,
      data(){
